03d0ed7fb475f4265d3fb5dd569d5e97d4390c8e
rizkiyanapp <rizkiyanapp@gmail.com>
2017-06-24 15:45:25 +0700

    constructing...

--- app/src/main/java/com/example/rizkiyanapp/themoviedb/controller/SplashScreen.java
+++ app/src/main/java/com/example/rizkiyanapp/themoviedb/controller/SplashScreen.java
@@ -1,9 +1,11 @@
-package com.example.rizkiyanapp.themoviedb;
+package com.example.rizkiyanapp.themoviedb.controller;
 
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.rizkiyanapp.themoviedb.R;
 
 public class SplashScreen extends AppCompatActivity {
 
